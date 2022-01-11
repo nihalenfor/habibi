b28f226302996e8f38d3d91a4a17e697857620ad
nihalenfor <kurumbathodinihal@gmail.com>
2022-01-11 11:01:03 +0530

    version2

--- paris/settings.py
+++ paris/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sk0qes)iaf2(oo8p=cx$o^iull24(hd-*0e$8%w$vvmymh6qi8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
